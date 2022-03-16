b6a08173fc7672f85cb9916d8113d3ff5c9c8bf3
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2022-03-16 10:21:51 +0100

    Stage 1/6: Encrypted

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -2,6 +2,23 @@ package encryptdecrypt;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        StringBuilder sb = new StringBuilder("we found a treasure!");
+
+        sb = encrypt(sb);
+
+        System.out.println(sb);
+    }
+
+    static StringBuilder encrypt(StringBuilder sb) {
+
+        for (int i = 0; i < sb.length(); i++) {
+
+            if (sb.charAt(i) >= 'a' && sb.charAt(i) <= 'z') {
+
+                sb.replace(i,i+1, String.valueOf((char) ('z' - sb.charAt(i) + 'a')));
+            }
+        }
+        return sb;
     }
 }
