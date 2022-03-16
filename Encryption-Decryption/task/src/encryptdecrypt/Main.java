de5dddba363476973abcc0773ffdbf6b46ddfd54
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2022-03-16 16:45:53 +0100

    Stage 6/6: Choices, choices

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -10,8 +10,11 @@ public class Main {
     public static void main(String[] args) {
 
         String mode = "enc";
+        String algorithm = "shift";
         int key = 0;
         StringBuilder data = new StringBuilder("");
+
+        Algorithm dataConversionAlgorithm = null;
 
         boolean inIsProvided = false;
         boolean outIsProvided = false;
@@ -56,6 +59,11 @@ public class Main {
                     outputFilePath = args[i + 1];
                     i += 1;
                     break;
+
+                case "-alg" :
+                    algorithm = args[i + 1];
+                    i += 1;
+                    break;
             }
         }
 
@@ -66,10 +74,22 @@ public class Main {
         }
 
         if ("enc".equals(mode)) {
-            encrypt(data, key);
-        } else {
-            decrypt(data, key);
+
+            if ("shift".equals(algorithm)) {
+                dataConversionAlgorithm = new EncryptionShift();
+            } else if ("unicode".equals(algorithm)) {
+                dataConversionAlgorithm = new EncryptionUnicode();
+            }
+        } else if ("dec".equals(mode)) {
+
+            if ("shift".equals(algorithm)) {
+                dataConversionAlgorithm = new DecryptionShift();
+            } else if ("unicode".equals(algorithm)) {
+                dataConversionAlgorithm = new DecryptionUnicode();
+            }
         }
+
+        data = dataConversionAlgorithm.execute(data, key);
 
         if (outIsProvided) {
 
@@ -83,24 +103,94 @@ public class Main {
             System.out.println(data);
         }
     }
+}
 
-    static StringBuilder encrypt(StringBuilder sb, int key) {
+interface Algorithm {
+    StringBuilder execute(StringBuilder data, int key);
+}
 
-        for (int i = 0; i < sb.length(); i++) {
+class DecryptionShift implements Algorithm {
 
-            char c = (char) (sb.charAt(i) + key);
-            sb.replace(i, i + 1, String.valueOf(c));
+    @Override
+    public StringBuilder execute(StringBuilder data, int key) {
+
+        for (int i = 0; i < data.length(); i++) {
+
+            char c = data.charAt(i);
+
+            if (data.charAt(i) >= 'A' && data.charAt(i) <= 'Z') {
+
+                c = (char) (c - key);
+                if (c < 'A') {
+                    char temp = (char) ('A' - c);
+                    c = (char) (91 - temp);
+                }
+            } else if (data.charAt(i) >= 'a' && data.charAt(i) <= 'z') {
+
+                c = (char) (c - key);
+                if (c < 'a') {
+                    char temp = (char) ('a' - c);
+                    c = (char) (123 - temp);
+                }
+            }
+            data.replace(i, i+1, String.valueOf(c));
         }
-        return sb;
+        return data;
     }
+}
 
-    static StringBuilder decrypt(StringBuilder sb, int key) {
+class DecryptionUnicode implements Algorithm {
 
-        for (int i = 0; i < sb.length(); i++) {
+    @Override
+    public StringBuilder execute(StringBuilder data, int key) {
 
-            char c = (char) (sb.charAt(i) - key);
-            sb.replace(i, i + 1, String.valueOf(c));
+        for (int i = 0; i < data.length(); i++) {
+
+            char c = (char) (data.charAt(i) - key);
+            data.replace(i, i + 1, String.valueOf(c));
         }
-        return sb;
+        return data;
+    }
+}
+
+class EncryptionShift implements Algorithm {
+
+    @Override
+    public StringBuilder execute(StringBuilder data, int key) {
+
+        for (int i = 0; i < data.length(); i++) {
+
+            char c = data.charAt(i);
+
+            if (data.charAt(i) >= 'A' && data.charAt(i) <= 'Z') {
+
+                c = (char) (c + key);
+                if (c > 'Z') {
+                    c = (char) (c - 90 + 64);
+                }
+            } else if (data.charAt(i) >= 'a' && data.charAt(i) <= 'z') {
+
+                c = (char) (c + key);
+                if (c > 'z') {
+                    c = (char) (c - 122 + 96);
+                }
+            }
+            data.replace(i, i+1, String.valueOf(c));
+        }
+        return data;
+    }
+}
+
+class EncryptionUnicode implements Algorithm {
+
+    @Override
+    public StringBuilder execute(StringBuilder data, int key) {
+
+        for (int i = 0; i < data.length(); i++) {
+
+            char c = (char) (data.charAt(i) + key);
+            data.replace(i, i + 1, String.valueOf(c));
+        }
+        return data;
     }
 }
